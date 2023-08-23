201d0d1c3cd6757667ab3f4d41f2ee7e5094a9cf
Ayush pandey <ayushpandey939@gmail.com>
2023-08-23 23:01:41 +0530

    Delete src directory

--- src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import tasksSlice from './tasksSlice'
-
-const store = configureStore({
-  reducer: tasksSlice,
-  devTools:true,
-})
-
-export default store
